b1d322492c314848bccd01189a7b4afd6876f5a1
christoffer <christoffer.thondrup@gmail.com>
2019-07-27 10:52:54 +0200

    Showing preview prospect data when clicked on marker

--- myUI.js
+++ myUI.js
@@ -1,7 +1,46 @@
 import { getItem } from './myelastic'
 
 export async function showPropsectPreview(id) {
+  const prospectElem = document.getElementById('previewProspect');
+  prospectElem.style.display = 'block';
+
+  const prospectCloseElem = document.getElementById('previewProspectClose');
+  prospectCloseElem.addEventListener("click", function() {
+    closePropsectPreview();
+  });
+
   const item = await getItem(id);
-  console.log(item);
-  window.open(id);
+
+  if(item.images.length > 0) {
+    const image = new Image();
+    image.onload = function() {
+      const imageElem = document.getElementById('previewProspectImage');
+      imageElem.setAttribute("src" ,this.src);
+    }
+    image.src = item.images[0];
+  }
+
+  const linkElem = document.getElementById('previewProspectLink');
+  linkElem.setAttribute('href', item.link);
+
+  const priceElem = document.getElementById('previewProspectPrice');
+  priceElem.innerHTML = item.price + ",- per mnd.";
+
+  const areaElem = document.getElementById('previewProspectArea');
+  areaElem.innerHTML = item.area + "&#x33a1;";
+
+  const categoryElem = document.getElementById('previewProspectCategory');
+  categoryElem.innerHTML =  item.category;
+
+  const descriptionElem = document.getElementById('previewProspectDescription');
+  const maxDescriptionLength = 600;
+  descriptionElem.innerHTML = item.description.substring(0, maxDescriptionLength)
+  if(item.description.length > maxDescriptionLength) {
+    descriptionElem.innerHTML += "...";
+  }
+}
+
+export async function closePropsectPreview() {
+  const prospectElem = document.getElementById('previewProspect');
+  prospectElem.style.display = 'none';
 }
